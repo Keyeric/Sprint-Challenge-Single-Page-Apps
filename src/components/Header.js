94b7dfae99a3a97b5a6a34425e81176a3a80dc3d
Key Bristol <keyeric.bristol@gmail.com>
2020-01-31 18:22:53 +0000

    This sucks, i hate this

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header className="ui centered">
-      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
-    </header>
+      <nav class="navbar navbar-light bg-light"></nav>
+        <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+        <div className = "lookNice">
+        <Link to='/' >Home</Link>
+        <Link to='/characters/' >Characters</Link>
+        <Link to='/search/' >Search</Link>
+        </div>
+        </header>
   );
 }
